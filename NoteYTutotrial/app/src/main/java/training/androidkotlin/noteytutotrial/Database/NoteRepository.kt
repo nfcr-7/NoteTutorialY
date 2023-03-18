09aba6daee4298d095587e696c5156b75c4ccef3
Rabier <=>
2023-03-18 03:47:19 +0100

    ProjetTuto

---
+++ NoteYTutotrial/app/src/main/java/training/androidkotlin/noteytutotrial/Database/NoteRepository.kt
@@ -0,0 +1,22 @@
+package training.androidkotlin.noteytutotrial.Database
+
+import androidx.lifecycle.LiveData
+import training.androidkotlin.noteytutotrial.Models.Note
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    val allNotes : LiveData<List<Note>> = noteDao.getAllNotes()
+
+    suspend fun insert(note: Note){
+        noteDao.insert(note)
+    }
+
+    suspend fun delete(note: Note){
+        noteDao.delete(note)
+    }
+
+    suspend fun update(note: Note){
+        noteDao.update(note.id, note.title, note.note)
+    }
+
+}
---
+++ NoteYTutotrial/app/src/main/java/training/androidkotlin/noteytutotrial/utilities/constants.kt
@@ -0,0 +1,3 @@
+package training.androidkotlin.noteytutotrial.utilities
+
+const val DATABASE_NAME = "note_database"
